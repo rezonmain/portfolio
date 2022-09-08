d38917af815eb076dd27c138f641cd473aecf022
rezonmain <adiazgomez101@gmail.com>
2022-09-08 10:17:46 -0700

    populated work page

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -1,16 +1,29 @@
+import { Tech } from '../ProjectList/ProjectList';
+import { BsGithub, BsGlobe } from 'react-icons/bs';
+
 const ProjectCard = ({
-	children,
+	title,
+	description,
+	imgSrc,
+	techs,
+	webURL,
+	codeURL,
 	accentColor,
 }: {
-	children: JSX.Element;
+	title: string;
+	description: string;
+	imgSrc: string;
+	techs: Tech[];
+	webURL?: string;
+	codeURL: string;
 	accentColor: string;
 }) => {
 	return (
-		<div id='card-container' className='w-full cursor-pointer'>
+		<div id='card-container'>
 			<div
 				id='card-background'
 				style={{ backgroundColor: accentColor }}
-				className='group relative'
+				className='group relative h-full'
 			>
 				<div
 					id='top-left-corner'
@@ -22,16 +35,129 @@ const ProjectCard = ({
 					style={{ borderTop: `8px solid ${accentColor}` }}
 					className={`absolute scale-0 border-r-8 border-r-transparent bottom-0 right-0 origin-[center_bottom_0] group-hover:translate-x-2 group-hover:scale-100 transition-transform`}
 				></div>
+
 				<div
 					id='card-content'
-					// style={{ border: `4px outset ${accentColor}` }}
-					className='bg-neutral-800 w-full group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform p-4'
+					className='bg-neutral-800 w-full h-full group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform p-4 flex flex-col gap-5 justify-between'
 				>
-					{children}
+					<h2 className='font-semibold text-2xl'>{title}</h2>
+					<p className='text-neutral-400 text-lg'>
+						{description.toLowerCase()}
+					</p>
+					<div
+						id='tech-icons'
+						className='flex flex-row gap-2 bg-neutral-900 w-fit p-1 rounded-xl'
+					>
+						{techs.map((t, i) => (
+							<a
+								target='_blank'
+								key={i}
+								href={tech[t].url}
+								className='p-0.5 rounded-full'
+							>
+								<img
+									title={tech[t].title}
+									src={tech[t].src}
+									alt={tech[t].alt}
+									width='30px'
+								></img>
+							</a>
+						))}
+					</div>
+					<div id='links' className='flex flex-row gap-3 justify-end'>
+						<a target='_blank' href={codeURL}>
+							<div className='p-1 rounded-full hover:bg-neutral-700'>
+								<BsGithub size={30} title='Source code' />
+							</div>
+						</a>
+						{webURL && (
+							<a target='_blank' href={webURL}>
+								<div className='p-1 rounded-full hover:bg-neutral-700 transition-colors'>
+									<BsGlobe size={30} title='Website' />
+								</div>
+							</a>
+						)}
+					</div>
 				</div>
 			</div>
 		</div>
 	);
+};
+
+export const tech = {
+	react: {
+		src: 'https://raw.githubusercontent.com/Pedro-Murilo/icons-for-readme/main/.github/react-icon.svg',
+		alt: 'reactjs icon',
+		url: 'https://reactjs.org/',
+		title: 'React.js',
+	},
+	ts: {
+		src: 'https://raw.githubusercontent.com/Pedro-Murilo/icons-for-readme/main/.github/typescript-icon.svg',
+		alt: 'typescript icon',
+		url: 'https://www.typescriptlang.org',
+		title: 'Typescript',
+	},
+	next: {
+		src: 'https://raw.githubusercontent.com/Pedro-Murilo/icons-for-readme/main/.github/nextjs-icon.svg',
+		alt: 'nextjs icon',
+		url: 'https://nextjs.org',
+		title: 'Next.js',
+	},
+	d3: {
+		src: 'https://raw.githubusercontent.com/d3/d3-logo/master/d3.png',
+		alt: 'd3js icon',
+		url: 'https://d3js.org/',
+		title: 'D3.js',
+	},
+	tailwind: {
+		src: 'https://www.nstinfotech.com/wp-content/uploads/2020/10/tailwind-logo.png',
+		alt: 'tailwind css icon',
+		url: 'https://tailwindcss.com/',
+		title: 'Tailwind CSS',
+	},
+	three: {
+		// src: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Three.js_Icon.svg',
+		src: '/img/three.png',
+		alt: 'threejs icon',
+		url: 'https://threejs.org/',
+		title: 'Three.js',
+	},
+	js: {
+		src: 'https://raw.githubusercontent.com/Pedro-Murilo/icons-for-readme/main/.github/js-icon.svg',
+		alt: 'javascript icon',
+		url: 'https://developer.mozilla.org/en-US/docs/Web/javascript',
+		title: 'Javascript',
+	},
+	html: {
+		src: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+		alt: 'html icon',
+		url: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+		title: 'Html 5',
+	},
+	css: {
+		src: 'https://raw.githubusercontent.com/Pedro-Murilo/icons-for-readme/main/.github/css-icon.svg',
+		alt: 'css icon',
+		url: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+		title: 'CSS 3',
+	},
+	vba: {
+		src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-k8cyYC06VjI%2FXm9Uaj76SZI%2FAAAAAAAALkc%2FMqiKk07ZN2cJ9z1rlIxgI0WgAI3FWE7wACLcBGAsYHQ%2Fs1600%2F1920px-VB.NET_Logo.svg.png&f=1&nofb=1',
+		alt: 'vba icon',
+		url: 'https://docs.microsoft.com/en-us/office/vba/api/overview/',
+		title: 'Visual Basic',
+	},
+	mongo: {
+		src: 'https://avatars.githubusercontent.com/u/45120?s=200&v=4',
+		alt: 'mongodb icon',
+		url: 'https://www.mongodb.com/',
+		title: 'MongoDB',
+	},
+	docker: {
+		src: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png',
+		alt: 'docker icon',
+		url: 'https://www.docker.com/',
+		title: 'Docker',
+	},
 };
 
 export default ProjectCard;
--- components/ProjectList/ProjectList.tsx
+++ components/ProjectList/ProjectList.tsx
@@ -1,29 +1,104 @@
+import { motion } from 'framer-motion';
 import ProjectCard from '../ProjectCard/ProjectCard';
+
+export type Tech =
+	| 'js'
+	| 'css'
+	| 'html'
+	| 'react'
+	| 'ts'
+	| 'next'
+	| 'd3'
+	| 'tailwind'
+	| 'vba'
+	| 'mongo'
+	| 'docker'
+	| 'three';
+
+export type Project = {
+	title: string;
+	description: string;
+	imgSrc: string;
+	techs: Tech[];
+	webURL?: string;
+	codeURL: string;
+	accentColor: string;
+};
+
+const projects: Project[] = [
+	{
+		title: 'minecraft server scanner',
+		description:
+			'Front end for my minecraft server scanner, Browse scanned entries, look for particular servers or players, see players skins with the built-in 3D skin viewer',
+		imgSrc: '/img/swc.png',
+		techs: ['react', 'ts', 'next', 'tailwind', 'mongo', 'docker', 'three'],
+		webURL: 'https://mc-server-scanner.vercel.app/',
+		codeURL: 'https://github.com/rezonmain/mc-server-scanner',
+		accentColor: '#17A34A',
+	},
+	{
+		title: 'simple word clouds',
+		description:
+			'Generate simple wordclouds from text file or text input, It uses D3.js to generate the cloud',
+		imgSrc: '/img/swc.png',
+		techs: ['react', 'next', 'ts', 'd3', 'tailwind'],
+		webURL: 'https://simple-wordcloud.vercel.app/',
+		codeURL: 'https://github.com/rezonmain/simple-wordcloud',
+		accentColor: '#F5F5F5',
+	},
+
+	{
+		title: 'command line typetest',
+		description: 'Tests your speed and accuracy typing linux commands',
+		imgSrc: '/img/swc.png',
+		techs: ['react', 'js', 'css'],
+		webURL: 'https://rezonmain.github.io/command-line-typetest/',
+		codeURL: 'https://github.com/rezonmain/command-line-typetest',
+		accentColor: '#1C8394',
+	},
+	{
+		title: 'react games',
+		description: 'Simple games implemented in react',
+		imgSrc: '/img/swc.png',
+		techs: ['react', 'js', 'ts', 'css', 'next'],
+		webURL: 'https://react-games-two.vercel.app/',
+		codeURL: 'https://github.com/rezonmain/react-games',
+		accentColor: '#FACC15',
+	},
+	{
+		title: 'red wagon',
+		description: 'Website for the red wagon restaurant',
+		imgSrc: '/img/swc.png',
+		techs: ['html', 'js', 'css'],
+		webURL: 'https://rezonmain.github.io/redwagon/',
+		codeURL: 'https://github.com/rezonmain/redwagon',
+		accentColor: '#E0312F',
+	},
+	{
+		title: 'minesweeper for excel',
+		description:
+			'A clone of minesweeper written in VBA that runs in Microsoft Excel',
+		imgSrc: '/img/swc.png',
+		techs: ['vba'],
+		webURL: '',
+		codeURL: 'https://github.com/rezonmain/minesweeper-for-excel',
+		accentColor: '#0100FE',
+	},
+];
 
 const ProjectList = () => {
 	return (
 		<section id='my-work'>
 			{/* <h1 className='text-2xl glitched'>{'[ my work ]'}</h1> */}
-			<div className='mt-4 flex flex-col items-center gap-4 lg:grid lg:grid-cols-2'>
-				<ProjectCard accentColor='#F5F5F5'>
-					<h1>simple word clouds</h1>
-				</ProjectCard>
-				<ProjectCard accentColor='#17A34A'>
-					<h1>minecraft server scanner</h1>
-				</ProjectCard>
-				<ProjectCard accentColor='#1C8394'>
-					<h1>command line typetest</h1>
-				</ProjectCard>
-				<ProjectCard accentColor='#FACC15'>
-					<h1>react games</h1>
-				</ProjectCard>
-				<ProjectCard accentColor='#E0312F'>
-					<h1>red wagon</h1>
-				</ProjectCard>
-				<ProjectCard accentColor='#0100FE'>
-					<h1>minesweeper for excel</h1>
-				</ProjectCard>
-			</div>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				className='mt-4 flex flex-col gap-4 lg:grid lg:grid-cols-2'
+			>
+				{projects.map((p, i) => (
+					<ProjectCard key={i} {...p} />
+				))}
+			</motion.div>
 		</section>
 	);
 };
