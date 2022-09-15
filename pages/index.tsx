8d810bb18ae93b153e6445a2cc7fc66e27c781a4
rezonmain <adiazgomez101@gmail.com>
2022-09-14 20:53:47 -0700

    added robots.txt, SEO description, og

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,16 @@ const Home: NextPage = () => {
 		<>
 			<Head>
 				<title>rezonmain | front-end dev</title>
+				<meta
+					name='description'
+					content="rezonmain's portfolio website, check out my work, judge my code."
+					key='desc'
+				></meta>
+				<meta property='og:title' content="rezonmain's portfolio website" />
+				<meta
+					property='og:description'
+					content='Checkout out my work, judge my code.'
+				/>
 			</Head>
 			<Transition>
 				<Hero />
