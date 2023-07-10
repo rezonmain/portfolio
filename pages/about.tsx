23043eda531ba9be509b25616255c8720531288e
rezonmain <adiazgomez101@gmail.com>
2023-07-09 21:22:10 -0700

    feat(aboutMe): update about me page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,131 +1,125 @@
-import Head from 'next/head';
-import { ReactElement } from 'react';
-import ContentLayout from '../components/ContentLayout/ContentLayout';
-import TechIcons from '../components/TechIcons/TechIcons';
-import Transition from '../components/Transition/Transition';
-import { Tech } from '../lib/tech';
-import { NextPageWithLayout } from './_app';
+import Head from "next/head";
+import { ReactElement } from "react";
+import ContentLayout from "../components/ContentLayout/ContentLayout";
+import TechIcons from "../components/TechIcons/TechIcons";
+import Transition from "../components/Transition/Transition";
+import { Tech } from "../lib/tech";
+import { NextPageWithLayout } from "./_app";
 
 const About: NextPageWithLayout = () => {
-	return (
-		<>
-			<Head>
-				<title>rezonmain | about</title>
-			</Head>
-			<Transition>
-				<section id='text-container' className='max-w-2xl flex flex-col gap-10'>
-					<article>
-						<Sub>about me</Sub>
-						<Text>
-							<>
-								Hello, my name is Alejandro Díaz (aka{' '}
-								<span className='glitched dark:text-glitched-dark'>
-									rezonmain
-								</span>
-								) I&apos;m a <b>front-end developer</b> located in tijuana 🇲🇽, I
-								focus on responsive and mobile friendly UI, with the added goal
-								to make every website easy to use and accessible to everyone.
-								<Pbr />I have a technical degree in <b>electronics</b> and a
-								bachelor degree in <b>bioengineering</b>, but have dedicated the
-								last 2.5 years learning web development.
-								<Pbr />
-								I&apos;ve been programming since my early teens, started with{' '}
-								<b>C</b>, writing firmware for{' '}
-								<TextLink href='https://www.arduino.cc/'>arduino</TextLink>, PIC
-								and ATMEL based microcontrollers, then learned <b>matlab</b> and{' '}
-								<b>python</b> in college, in the last year of it I learned{' '}
-								<b>javascript</b> to write websites and back-end services for my
-								school IoT and research projects. I also have a bit of
-								experience writing <b>VBA</b> for automating Microsoft office
-								documents.
-							</>
-						</Text>
-					</article>
-					<article>
-						<Sub>tech stack</Sub>
-						<Text>
-							<>
-								I use <b>vscode</b> as my text editor, my front-end framework of
-								choice is <b>react</b> which I pair it with{' '}
-								<TextLink href='https://nextjs.org/'>
-									<b>next.js</b>
-								</TextLink>{' '}
-								for{' '}
-								<TextLink href='https://nextjs.org/learn/foundations/how-nextjs-works/rendering'>
-									SSR
-								</TextLink>
-								. Though I&apos;m not afraid of writing simple websites with
-								vanilla JS
-								<Pbr />
-								<b>Typescript</b> is my language of choice for both front-end
-								and when I need to write a little bit of back-end code, which is
-								usually in the form of a{' '}
-								<TextLink href='https://trpc.io/'>
-									<b>tRPC</b>
-								</TextLink>{' '}
-								api, and involves a <b>mongoDB</b> database. For styling I use
-								Tailwind CSS, these technologies &#40;Typescript, tRPC,
-								Tailwind&#41; form the T3 stack popularized by{' '}
-								<TextLink href='https://twitter.com/t3dotgg'>Theo.</TextLink>
-							</>
-						</Text>
-						{/* All icons */}
-						<br />
-						<TechIcons techs={Object.values(Tech)} />
-					</article>
-				</section>
-			</Transition>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>rezonmain | about</title>
+      </Head>
+      <Transition>
+        <section id="text-container" className="max-w-2xl flex flex-col gap-10">
+          <article>
+            <Sub>about me</Sub>
+            <Text>
+              <>
+                hi, my name is Alejandro Díaz (aka{" "}
+                <span className="glitched dark:text-glitched-dark">
+                  rezonmain
+                </span>
+                ) I&apos;m a <b>software engineer</b> located in tijuana 🇲🇽, I
+                focus on front-end development making responsive and mobile
+                friendly webapps.
+                <Pbr />I have a technical degree in <b>electronics</b> and a
+                bachelor degree in <b>bioengineering</b>, but have dedicated the
+                last 5 years learning (and working in) everything web
+                development.
+              </>
+            </Text>
+          </article>
+          <article>
+            <Sub>work xp</Sub>
+            <Text>
+              <ul className="list-disc">
+                <li className="ml-8">
+                  React developer @{" "}
+                  <TextLink href="https://www.arkusnexus.com/">
+                    ArkusNexus
+                  </TextLink>{" "}
+                  🤓
+                </li>
+                <li className="ml-8">
+                  Engineer @{" "}
+                  <TextLink href="https://drata.com/">Drata</TextLink> 😎
+                </li>
+              </ul>
+            </Text>
+          </article>
+          <article>
+            <Sub>stack</Sub>
+            <Text>
+              <>
+                <strong>Typescript</strong> is my main language, and I know{" "}
+                <strong>React</strong> like the back of my hand, but of course I
+                sometimes cheat on it with Vue and Svelte, Next.js is the
+                framework I have most experience in but I&apos;ve built webapps
+                using the most popular JS libraries and frameworks, I love tech
+                and I love trying out the newest and latest tools out there,
+                just for fun.
+              </>
+            </Text>
+            {/* All icons */}
+            <br />
+            <TechIcons techs={Object.values(Tech)} />
+          </article>
+        </section>
+      </Transition>
+    </>
+  );
 };
 
 export const Sub = ({
-	children,
-	id,
+  children,
+  id,
 }: {
-	children: string | JSX.Element;
-	id?: string;
+  children: string | JSX.Element;
+  id?: string;
 }) => {
-	return (
-		<h2 id={id ? id : 'sub-title'} className='text-3xl mb-5'>
-			{children}
-		</h2>
-	);
+  return (
+    <h2 id={id ? id : "sub-title"} className="text-3xl mb-5">
+      {children}
+    </h2>
+  );
 };
 
 export const Text = ({ children }: { children: string | JSX.Element }) => {
-	return <p className='text-xl font-extralight'>{children}</p>;
+  return <p className="text-xl font-extralight">{children}</p>;
 };
 
 export const TextLink = ({
-	children,
-	href,
+  children,
+  href,
 }: {
-	children: string | JSX.Element;
-	href: string;
+  children: string | JSX.Element;
+  href: string;
 }) => {
-	return (
-		<a
-			target='_blank'
-			rel='noreferrer'
-			className='text-blue-500 hover:underline'
-			href={href}
-		>
-			{children}
-		</a>
-	);
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer"
+      className="text-blue-500 hover:underline"
+      href={href}
+    >
+      {children}
+    </a>
+  );
 };
 
 const Pbr = () => (
-	<>
-		<br></br>
-		<br></br>
-	</>
+  <>
+    <br></br>
+    <br></br>
+  </>
 );
 
 // Opt-in shared layout
 About.getLayout = function getLayout(page: ReactElement) {
-	return <ContentLayout>{page}</ContentLayout>;
+  return <ContentLayout>{page}</ContentLayout>;
 };
 
 export default About;
